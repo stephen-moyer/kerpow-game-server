1a8e01dd665d4531d282169883aa540a855d6a81
Stephen Moyer <steveadoo32@gmail.com>
2016-05-31 20:55:45 -0400

    Combined validation server into the base server

--- src/main/java/com/kerpow/games/server/Server.java
+++ src/main/java/com/kerpow/games/server/Server.java
@@ -2,13 +2,23 @@ package com.kerpow.games.server;
 
 import com.kerpow.games.packets.Packet;
 import com.kerpow.games.packets.PacketProcessor;
+import com.kerpow.games.server.validation.Validator;
 import io.netty.channel.Channel;
 import io.netty.util.AttributeKey;
+
+import java.util.LinkedList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public abstract class Server<TInfo extends ServerInfo> implements PacketProcessor.ProcessPipeline {
 
     public static final AttributeKey<Player> PLAYER_ATTRIBUTE_KEY = AttributeKey.newInstance("Channel.player");
+    public static final AttributeKey<Boolean> VALIDATED_ATTR_KEY = AttributeKey.newInstance("Channel.validated");
 
+    protected final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+    private final LinkedList<Validator> validators;
     private final PacketProcessor packetProcessor;
     private final TInfo serverInfo;
 
@@ -16,20 +26,40 @@ public abstract class Server<TInfo extends ServerInfo> implements PacketProcesso
         this.serverInfo = serverInfo;
         this.packetProcessor = packetProcessor;
         this.packetProcessor.setProcessPipeline(this);
+        this.validators = new LinkedList<>();
     }
 
     public Object transformSender(Object sender, Packet packet) {
         return ((Channel) sender).attr(PLAYER_ATTRIBUTE_KEY).get();
     }
 
-    public boolean checkMessage(Object player, Packet packet, Object message) {
-        return player != null;
+    public final boolean checkMessage(Object sender, Packet packet, Object message) {
+        Player player = (Player) sender;
+        if (serverInfo.validate) {
+            Boolean validated = player.channel.attr(VALIDATED_ATTR_KEY).get();
+            if (validated == null || !validated) {
+                //trigger validators
+                tryValidate(player, packet, message);
+                return false;
+            }
+        }
+        return true;
     }
 
     public final void onConnect(Channel channel) {
         Player player = createPlayer(channel);
         channel.attr(PLAYER_ATTRIBUTE_KEY).set(player);
-        connected(player);
+        if (serverInfo.validate) {
+            executorService.schedule(() -> {
+                Boolean validated = player.channel.attr(VALIDATED_ATTR_KEY).get();
+                if (validated == null || !validated) {
+                    player.channel.attr(Server.PLAYER_ATTRIBUTE_KEY).remove();
+                    player.channel.close();
+                }
+            }, serverInfo.validationTimeout, TimeUnit.MILLISECONDS);
+        } else {
+            connected(player);
+        }
     }
 
     public final void onDisconnect(Channel channel) {
@@ -38,6 +68,26 @@ public abstract class Server<TInfo extends ServerInfo> implements PacketProcesso
             return;
         }
         disconnected(player);
+    }
+
+    private void tryValidate(Player player, Packet packet, Object message) {
+        for (Validator validator : validators) {
+            if (!validator.matches(packet.opcode)) {
+                continue;
+            }
+            validator.validate(player, packet.opcode, message).thenAccept(validated -> {
+                if (!validated) {
+                    return;
+                }
+                onValidated(player);
+            });
+            return;
+        }
+    }
+
+    private final void onValidated(Player player) {
+        player.channel.attr(VALIDATED_ATTR_KEY).set(true);
+        connected(player);
     }
 
     public final PacketProcessor getPacketProcessor() {
--- src/main/java/com/kerpow/games/server/ServerInfo.java
+++ src/main/java/com/kerpow/games/server/ServerInfo.java
@@ -3,6 +3,8 @@ package com.kerpow.games.server;
 public class ServerInfo {
 
     public final int port;
+    public final boolean validate;
+    public final int validationTimeout;
 
     /**
      * Constructs a ServerInfo that will tell the server listen on the provided port
@@ -10,6 +12,13 @@ public class ServerInfo {
      */
     public ServerInfo(int port) {
         this.port = port;
+        this.validate = false;
+        this.validationTimeout = 0;
+    }
+    public ServerInfo(int port, int validationTimeout) {
+        this.port = port;
+        this.validate = true;
+        this.validationTimeout = validationTimeout;
     }
 
 }
